964e6978b1236a4a0e953e2fbbbf8842017fcecd
Deividy <deividyosantos27@gmail.com>
2022-04-01 14:40:22 -0400

    create api

---
+++ api/app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserCreateRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+
+class UserController extends Controller
+{
+    public function create(UserCreateRequest $request): JsonResponse
+    {
+        $user = User::query()->create($request->validationData());
+
+        return response()->json([
+            'message' => 'user created successfully',
+            'data' => [
+                'name' => $user->name,
+                'email' => $user->email
+            ]
+        ],  201);
+    }
+}
---
+++ api/tests/Feature/app/Http/Controllers/UserControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature\app\Http\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_should_be_able_register_a_user()
+    {
+        $response = $this->withHeaders(['Accept' => 'application/json'])
+            ->post(Route('user.create'), [
+                'name' => 'test',
+                'email' => 'test@example.com',
+                'password' => 'password'
+            ]);
+
+        $response->assertStatus(201);
+        $response->assertExactJson([
+            'message' => 'user created successfully',
+            'data' => [
+                'name' => 'test',
+                'email' => 'test@example.com',
+            ]
+        ]);
+    }
+
+    public function test_data_should_be_required()
+    {
+        $response = $this->withHeaders(['Accept' => 'application/json'])
+            ->post(Route('user.create'), []);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_email_should_be_valid_email()
+    {
+        $user = [
+            'name' => 'test',
+            'email' => 'test.email',
+            'password' => 'password'
+        ];
+
+        $response = $this->withHeaders(['Accept' => 'application/json'])
+            ->post(Route('user.create'), $user);
+
+        $response->assertStatus(422);
+    }
+
+}
